f095bbce88dedc24a9fd5d8d2f688a99a02e73e7
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2022-05-11 11:52:44 +0200

    feat: add main file for execution

---
+++ main.py
@@ -0,0 +1,15 @@
+from environments.ReplaceTokenEnvironment import ReplaceTokenEnvironment
+
+if __name__ == '__main__':
+    env = ReplaceTokenEnvironment()
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+        env.render()
+
+        if done:
+            #env.reset()
+            break
+
+    env.close()
